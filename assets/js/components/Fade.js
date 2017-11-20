ea6eb15dbde6393f83926ced222e795498cde886
Ross Basarevych <basarevych@gmail.com>
2017-11-20 06:44:10 +0200

    Import vars from styles

--- assets/js/components/Fade.js
+++ assets/js/components/Fade.js
@@ -1,12 +1,13 @@
 'use strict';
 
+import style from '../../styles/index.scss';
 import React from 'react';
 import { CSSTransition } from 'react-transition-group';
 
 const Fade = ({ children, ...props }) => (
   <CSSTransition
     {...props}
-    timeout={300}
+    timeout={parseInt(style.fadeDuration)}
     classNames="fade"
   >
     {children}
